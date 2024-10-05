6d0c9e850f9a3a8e241c5bb74bcb904b1556a9ef
sanjidamukti <sanjidamukti147146@gmail.com>
2024-10-05 12:51:48 +0600

    tailwind login formt

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    // fontSize: {
+    //   'mim': ['5rem']
+    // },
+    fontFamily: {
+    'display': ['Oswald']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
